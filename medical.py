1aabe9b649a149cda030ed50b2c4e477c1823722
Arnold <arnoldjulian90@gmail.com>
2015-09-08 16:38:17 +0100

    Rotated Image

--- medical.py
+++ medical.py
@@ -6,10 +6,12 @@ import scipy.ndimage.interpolation
 import matplotlib.pyplot as plt
 
 phantom = scipy.misc.imread("Phantom.jpg")
-
-#for x in range(0,180):
-plt.imshow(phantom) 
+plt.imshow(phantom)
 plt.show()
+for x in range(0,180):
+	rotatedImage = scipy.ndimage.interpolation.rotate(phantom,1,3, 
+reshap=False, mode= 'constant', cval=0)
 
-#scipy.misc.imsave("Image.jpg",image)
+
+
 
